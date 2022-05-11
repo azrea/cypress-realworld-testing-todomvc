2aa960b48e42eedc8a62f84585e8c4b6776ee160
MarianaES <mariana.enriquez.s@gmail.com>
2022-05-10 20:49:00 -0500

    First use of cypress commands

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -14,10 +14,28 @@ describe("React TodoMVC", () => {
     cy.get(".todo-list li").eq(0).find("label").should("contain", TODO_ITEM_ONE)
   }),
     it("can add three todos", () => {
-      cy.get(".new-todo").type(`${TODO_ITEM_ONE}{enter}`)
-      cy.get(".new-todo").type(`${TODO_ITEM_TWO}{enter}`)
-      cy.get(".new-todo").type(`${TODO_ITEM_THREE}{enter}`)
-
+      cy.createDefaultTodos()
       cy.get(".todo-list li").should("have.length", 3)
+    }),
+    it("should append new items to the bottom of the list", () => {
+      cy.createDefaultTodos()
+
+      cy.get(".todo-list li")
+        .eq(0)
+        .find("label")
+        .should("contain", TODO_ITEM_ONE)
+
+      cy.get(".todo-list li")
+        .eq(1)
+        .find("label")
+        .should("contain", TODO_ITEM_TWO)
+
+      cy.get(".todo-list li")
+        .eq(2)
+        .find("label")
+        .should("contain", TODO_ITEM_THREE)
+
+      cy.get(".todo-count").should("contain", "3 items left")
+      cy.get(".todo-count").contains("3 items left")
     })
 })
